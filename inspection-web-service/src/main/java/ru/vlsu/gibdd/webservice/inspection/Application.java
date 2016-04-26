e54bf35742dad175f3b2ca24e38689e8ba1ed83b
Victor Zhivotikov <deathman1912@gmail.com>
2016-04-27 00:37:03 +0300

    Add war packaging

--- inspection-web-service/src/main/java/ru/vlsu/gibdd/webservice/inspection/Application.java
+++ inspection-web-service/src/main/java/ru/vlsu/gibdd/webservice/inspection/Application.java
@@ -2,6 +2,8 @@ package ru.vlsu.gibdd.webservice.inspection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -22,9 +24,14 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 		ru.vlsu.gibdd.webservice.inspection.Application.class
 })
 @EnableTransactionManagement
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 }
--- signature-web-service/src/main/java/ru/vlsu/gibdd/webservice/signature/Application.java
+++ signature-web-service/src/main/java/ru/vlsu/gibdd/webservice/signature/Application.java
@@ -2,6 +2,8 @@ package ru.vlsu.gibdd.webservice.signature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -21,9 +23,14 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 })
 @EnableJpaAuditing
 @EnableTransactionManagement
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
 	}
 }
